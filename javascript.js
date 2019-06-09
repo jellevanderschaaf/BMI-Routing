458445dd80e30ed5cad942fccdbc2c9d38dd15dc
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-09 23:43:35 +0200

    Added weight and height variables to calculate bmi.

--- javascript.js
+++ javascript.js
@@ -5,10 +5,18 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req, res) {
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var height2 = height * height;
+
+    var bmi = weight / height2;
+
+    res.send("Your BMI is " + bmi);
 
 });
 
